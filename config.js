ece8acbdd42122a4a4672d7b94a18b9cb93f7808
MaNnU <mannuspamm@gmail.com>
2026-07-05 17:17:45 +0000

    bug config typo

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // Configurazione app - unico punto da modificare quando cambia il deploy Apps Script.
 const CONFIG = {
-  // Sostituisci con l'URL del tuo Web App Apps Script (Deploy > Nuovo deployment > Web app)
-  APPS_SCRIPT_URL: "https://script.https://script.google.com/macros/s/AKfycbyFUAWdQeZvDpLkxl0vcgjlOq4JxpsbtoW5Jqa5MC8m/dev.com/macros/s/INCOLLA_QUI_IL_TUO_ID/exec"
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyFUAWdQeZvDpLkxl0vcgjlOq4JxpsbtoW5Jqa5MC8m/exec"
 };
